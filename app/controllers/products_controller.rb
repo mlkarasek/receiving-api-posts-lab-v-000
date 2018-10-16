5ccbdac66a5b9bc409b9079274138f87e4d5b7fd
Monica  Karasek <mlkarasek@gmail.com>
2018-10-16 19:41:34 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @product = Product.create(product_params)
+    redirect_to products_path(@product)
   end
 
   def show
